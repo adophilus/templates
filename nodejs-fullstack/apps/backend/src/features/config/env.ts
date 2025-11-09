694a1e8608352e3d3ac3c13f9bfbd63c180b153b
Adophilus <uchenna19of@gmail.com>
2025-11-09 20:03:34 +0100

    chore: updated env type for backend

--- nodejs-fullstack/apps/backend/src/features/config/env.ts
+++ nodejs-fullstack/apps/backend/src/features/config/env.ts
@@ -1,39 +1,43 @@
 import { z } from 'zod'
 
-const sqliteDatabaseEnv = z.object({
-  DATABASE_PROVIDER: z.literal('sqlite'),
-  DATABASE_PREFIX: z.string().optional(),
-  DATABASE_URL: z.string()
+const baseEnv = z.object({
+  NODE_ENV: z.enum(['production', 'staging', 'development', 'test']),
+  SERVER_PORT: z.coerce.number().min(0).max(65535),
+  MAIL_URL: z.url(),
+  MAIL_SENDER_NAME: z.string(),
+  MAIL_SENDER_EMAIL: z.email(),
+  MAIL_SUPPORT_NAME: z.string(),
+  MAIL_SUPPORT_EMAIL: z.email(),
+  SERVER_URL: z.string(),
+  AUTH_TOKEN_SECRET: z.string().min(32)
 })
 
-const pgDatabaseEnv = z.object({
-  DATABASE_PROVIDER: z.literal('postgres'),
-  DATABASE_PREFIX: z.string().optional(),
-  DATABASE_URL: z.url()
-})
+const sqliteDatabaseEnv = baseEnv.safeExtend(
+  z.object({
+    DATABASE_PROVIDER: z.literal('sqlite'),
+    DATABASE_PREFIX: z.string().optional(),
+    DATABASE_URL: z.string()
+  })
+)
 
-const pgliteDatabaseEnv = z.object({
-  DATABASE_PROVIDER: z.literal('pglite'),
-  DATABASE_PREFIX: z.string().optional(),
-  DATABASE_URL: z.string()
-})
+const pgDatabaseEnv = baseEnv.safeExtend(
+  z.object({
+    DATABASE_PROVIDER: z.literal('postgres'),
+    DATABASE_PREFIX: z.string().optional(),
+    DATABASE_URL: z.url()
+  })
+)
 
-const databaseEnv = z.discriminatedUnion('DATABASE_PROVIDER', [
+const pgliteDatabaseEnv = baseEnv.safeExtend(
+  z.object({
+    DATABASE_PROVIDER: z.literal('pglite'),
+    DATABASE_PREFIX: z.string().optional(),
+    DATABASE_URL: z.string()
+  })
+)
+
+export const env = z.union([
   sqliteDatabaseEnv,
   pgDatabaseEnv,
   pgliteDatabaseEnv
 ])
-
-export const env = z
-  .object({
-    NODE_ENV: z.enum(['production', 'staging', 'development', 'test']),
-    SERVER_PORT: z.coerce.number().min(0).max(65535),
-    MAIL_URL: z.url(),
-    MAIL_SENDER_NAME: z.string(),
-    MAIL_SENDER_EMAIL: z.email(),
-    MAIL_SUPPORT_NAME: z.string(),
-    MAIL_SUPPORT_EMAIL: z.email(),
-    SERVER_URL: z.string(),
-    AUTH_TOKEN_SECRET: z.string().min(32)
-  })
-  .extend(databaseEnv.shape)
