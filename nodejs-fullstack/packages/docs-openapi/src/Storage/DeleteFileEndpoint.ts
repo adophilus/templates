84a1214d14676fbf29c175085e600cd9fbb270fa
Adophilus <uchenna19of@gmail.com>
2025-11-15 11:20:23 +0100

    chore: added endpoint definition for the delete file endpoint

---
+++ nodejs-fullstack/packages/docs-openapi/src/Storage/DeleteFileEndpoint.ts
@@ -0,0 +1,31 @@
+import { HttpApiEndpoint, HttpApiSchema } from '@effect/platform'
+import { StatusCodes } from 'http-status-codes'
+import { OpenApi } from '@effect/platform'
+import FileNotFoundError from '../common/FileNotFoundError'
+import UnauthorizedError from '../common/UnauthorizedError'
+import UnexpectedError from '../common/UnexpectedError'
+import { Schema } from 'effect'
+import Id from '../common/Id'
+
+const Request = Schema.Struct({
+  fileId: Id
+}).annotations({
+  description: 'Delete file request path parameters'
+})
+
+const Success = Schema.Struct({
+  code: Schema.Literal('FILE_DELETED'),
+  message: Schema.String
+})
+
+const DeleteFileEndpoint = HttpApiEndpoint.delete('deleteFile', '/storage/:fileId')
+  .setPath(Request)
+  .addSuccess(Success, { status: StatusCodes.OK })
+  .addError(FileNotFoundError, {
+    status: StatusCodes.NOT_FOUND
+  })
+  .addError(UnauthorizedError, { status: StatusCodes.UNAUTHORIZED })
+  .addError(UnexpectedError, { status: StatusCodes.INTERNAL_SERVER_ERROR })
+  .annotate(OpenApi.Description, 'Delete a media file by ID')
+
+export default DeleteFileEndpoint
--- nodejs-fullstack/packages/docs-openapi/src/Storage/index.ts
+++ nodejs-fullstack/packages/docs-openapi/src/Storage/index.ts
@@ -1,9 +1,11 @@
 import { HttpApiGroup } from '@effect/platform'
 import UploadMediaEndpoint from './UploadMediaEndpoint'
 import GetFileEndpoint from './GetFileEndpoint'
+import DeleteFileEndpoint from './DeleteFileEndpoint'
 
 const StorageApi = HttpApiGroup.make('Storage')
   .add(UploadMediaEndpoint)
   .add(GetFileEndpoint)
+  .add(DeleteFileEndpoint)
 
 export default StorageApi
