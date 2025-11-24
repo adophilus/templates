580d48e695a67c676c20046df4692de938c190a6
Adophilus <uchenna19of@gmail.com>
2025-11-24 15:50:20 +0100

    chore: imported code with updated names

--- nodejs-fullstack/apps/backend/src/temp.ts
+++ nodejs-fullstack/apps/backend/src/temp.ts
@@ -4,16 +4,24 @@ import { Api } from '@nodejs-fullstack-template/docs-openapi'
 import { createServer } from 'node:http'
 import StorageApiLive from './features/storage/route'
 import { Layer } from 'effect'
-import { sqliteStorageLive } from './features/storage/service'
-import { kyselyStorageRepositoryLive } from './features/storage/repository'
-import { sqliteKyselyClientLive } from './features/database/kysely/db/sqlite'
+import { SqliteStorageLive } from './features/storage/service'
+import { KyselyStorageRepositoryLive } from './features/storage/repository'
+import { SqliteKyselyClientLive } from './features/database/kysely/db/sqlite'
 import { DevTools } from '@effect/experimental'
+import AuthApiLive from './features/auth/route'
+import { KyselyAuthUserRepositoryLive } from './features/auth/repository'
+
+const AuthLayer = AuthApiLive.pipe(Layer.provide(KyselyAuthUserRepositoryLive))
+
+const StorageLayer = StorageApiLive.pipe(
+  Layer.provide(SqliteStorageLive),
+  Layer.provide(KyselyStorageRepositoryLive)
+)
 
 const ApiLive = HttpApiBuilder.api(Api).pipe(
-  Layer.provide(StorageApiLive),
-  Layer.provide(sqliteStorageLive),
-  Layer.provide(kyselyStorageRepositoryLive),
-  Layer.provide(sqliteKyselyClientLive)
+  Layer.provide(AuthLayer),
+  Layer.provide(StorageLayer),
+  Layer.provide(SqliteKyselyClientLive)
 )
 
 const HttpLive = HttpApiBuilder.serve(HttpMiddleware.logger).pipe(
