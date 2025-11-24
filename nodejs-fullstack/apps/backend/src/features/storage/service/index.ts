5b543bd3ea6c79e68c050cdfa7d984307ef3c350
Adophilus <uchenna19of@gmail.com>
2025-11-24 15:50:57 +0100

    chore: updated re-exports

--- nodejs-fullstack/apps/backend/src/features/storage/service/index.ts
+++ nodejs-fullstack/apps/backend/src/features/storage/service/index.ts
@@ -1,9 +1,7 @@
-export * from "./error";
-export * from "./interface";
-export * from "./mock";
-export * from "./sqlite";
+export * from './error'
+export * from './interface'
+export * from './mock'
+export * from './sqlite'
 
-// Export the specific classes/functions that existing code might expect
-// These are the legacy exports that might be expected by bootstrap.ts
-export { MockStorage, MockStorageLive } from "./mock";
-export { sqliteStorageLive as createSqliteStorage, SqliteStorageLive } from "./sqlite";
+export { MockStorage, MockStorageLive } from './mock'
+export { SqliteStorageLive } from './sqlite'
