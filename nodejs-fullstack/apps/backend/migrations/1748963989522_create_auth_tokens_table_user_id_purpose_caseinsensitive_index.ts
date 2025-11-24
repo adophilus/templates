78caaa13a909c7ec1b04dec7d3bc4581b8cb6ef1
Adophilus <uchenna19of@gmail.com>
2025-11-24 15:48:53 +0100

    chore: fixed sql error in the migrations

--- nodejs-fullstack/apps/backend/migrations/1748963989522_create_auth_tokens_table_user_id_purpose_caseinsensitive_index.ts
+++ nodejs-fullstack/apps/backend/migrations/1748963989522_create_auth_tokens_table_user_id_purpose_caseinsensitive_index.ts
@@ -12,6 +12,5 @@ export async function up(db: Kysely<any>): Promise<void> {
 export async function down(db: Kysely<any>): Promise<void> {
   await db.schema
     .dropIndex('user_id_purpose_unique_caseinsensitive_idx')
-    .on('auth_tokens')
     .execute()
 }
